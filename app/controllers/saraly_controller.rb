64a7228050a1d5c50d22eaf76dfd26442cc45ded
umekishimaru <riki12344649@gmail.com>
2023-07-04 16:01:36 +0900

    slim導入

---
+++ app/controllers/saraly_controller.rb
@@ -0,0 +1,10 @@
+class SaralyController < ApplicationController
+  def home
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'saraly/home'
+  get 'saraly/index'
+  get 'saraly/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
