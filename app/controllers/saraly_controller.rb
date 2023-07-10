46b9fd61c7e3302d0f8fbf83611a3c1dc60ba8cb
umekishimaru <riki12344649@gmail.com>
2023-07-10 17:06:50 +0900

    logic完成

--- app/controllers/saraly_controller.rb
+++ app/controllers/saraly_controller.rb
@@ -8,11 +8,11 @@ class SaralyController < ApplicationController
   end
 
   def show
-    month =  params[:salary][:month]
+    @month =  params[:salary][:month]
     weekdays_goal = params[:salary][:weekdays]
     weekends_goal = params[:salary][:weekends]
     holidays = params[:salary][:holidays]
-    @salary = Salary.new(month,weekdays_goal,weekends_goal,holidays)
+    @salary = Salary.new(@month,weekdays_goal,weekends_goal,holidays)
 
   end
 end
