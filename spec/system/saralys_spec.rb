13a953d28b0ec797a79a47889b8aeea517704583
umekishimaru <riki12344649@gmail.com>
2023-07-13 10:59:24 +0900

    index_form_rspecテスト

--- spec/system/saralys_spec.rb
+++ spec/system/saralys_spec.rb
@@ -7,8 +7,17 @@ RSpec.describe "Saralys", type: :system do
 
 
   it 'calulate my salary' do 
-    visit 'saraly_index_path'
-    select ''
+    visit saraly_index_path
+    select '7月' , from: '月'
+    fill_in 'salary[weekdays]', with: '4000'
+    fill_in 'salary[weekends]', with: '6000'
+    fill_in 'salary[holidays]', with: '5'
+    click_button '送信'
+
+    
+
+
+    
   end
   
 end
